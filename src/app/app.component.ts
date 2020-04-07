d6f0d4ced9d96e6e6eb2dee99ca495d8c9e7dc65
smizoguchi <mizoguchy@gmail.com>
2020-04-07 10:04:55 -0500

    current iteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import restaurants from '../../restaurants.json';
-import { Post } from './post';
-import { Observable } from 'rxjs/Observable';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import restaurants from "../../restaurants.json";
+import { Post } from "./post";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html', 
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
+  name = "Angular";
+  public restList: { name: string }[] = restaurants;
 
-  name = 'Angular';
-  public restList:{name:string}[] = restaurants;
-
-  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com/todos/1'
+  readonly ROOT_URL = "https://jsonplaceholder.typicode.com/todos/1";
 
   posts: Observable<any>;
   newPost: Observable<any>;
@@ -28,18 +27,16 @@ export class AppComponent  {
     ceil: 100,
     step: 10,
     showTicks: true
-  }
+  };
 
   getPosts() {
-
-    console.log("Getting post")
-    this.posts = this.http.get(this.ROOT_URL + '/posts?userId=1')
+    console.log("Getting post");
+    this.posts = this.http.get(this.ROOT_URL + "/posts?userId=1");
   }
 
   sendPosts() {
     //send some data to the server
     //data to be sent over
-
 
     const data: Post = {
       id: 1,
@@ -47,9 +44,9 @@ export class AppComponent  {
       title: "haha",
       body: "hello",
       cost: "some data"
-    }
+    };
 
-    this.newPost = this.http.post(this.ROOT_URL + '/posts', data)
-    console.log("sending post")
+    this.newPost = this.http.post(this.ROOT_URL + "/posts", data);
+    console.log("sending post");
   }
 }
